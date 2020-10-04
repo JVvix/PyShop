3dc68e1c65b4c67d8568cb12ae26f1153cdaa532
JVvix <fayedotly@gmail.com>
2020-10-03 17:35:12 -0700

    can't get farther than the coupon code part because of errors. needs help

--- manage.py
+++ manage.py
@@ -13,3 +13,4 @@ if __name__ == '__main__':
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
--- pyshop/settings.py
+++ pyshop/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+django.setup()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
